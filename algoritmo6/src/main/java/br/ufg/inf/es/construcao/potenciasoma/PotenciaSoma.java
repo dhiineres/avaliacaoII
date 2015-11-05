8f96a937a97f45685419b583089cd40e7dc29e31
alunoinf <alunoinf@pc-649301.(none)>
2015-11-05 21:17:38 -0200

    Adicionado algoritmo Potencia

--- algoritmo6/src/main/java/br/ufg/inf/es/construcao/potenciasoma/PotenciaSoma.java
+++ algoritmo6/src/main/java/br/ufg/inf/es/construcao/potenciasoma/PotenciaSoma.java
@@ -1,50 +1,36 @@
 package br.ufg.inf.es.construcao.potenciasoma;
 
+import br.ufg.inf.es.construcao.produto.Produto;
+
 /**
- * Algoritmo para efetuar operações de exponenciação com o uso de funções de
- * produto pré estabelecidas;
+ * Algoritmo para efetuar operacoes de exponenciacao com o uso de funcoes de
+ * produto pre-estabelecidas;
  */
 public class PotenciaSoma {
 
     /**
-     * realiza a operação;
-     * @param x base da operação;
-     * @param y expoente da operação;
-     * @return o valor da exponenciação;
-     * @throws IllegalArgumentException caso ocorra uma entrada de parâmetro
+     * realiza a operacao;
+     * @param x base da operacao;
+     * @param y expoente da operacao;
+     * @return o valor da exponenciacao;
+     * @throws IllegalArgumentException caso ocorra uma entrada de parï¿½metro
      * inadequado
      * x deve ser um valor maior que 0, inclusive
      * y deve ser um valor maior que 0;
      */
     public static int potencia(int x, int y){
         if (x <= 0 || y < 1){
-            throw new IllegalArgumentException("Parâmetros de entrada inválidos");
+            throw new IllegalArgumentException("Parï¿½metros de entrada invï¿½lidos");
         }
 
         int i = 1;
         int p = x;
 
         while (i < y){
-            p = produto(p, x);
+            p = Produto.produto(p, x);
             i = i + 1;
         }
 
         return p;
-    }
-
-    public static int produto(int a, int b){
-        if (a < 0 || b < 0){
-            throw new IllegalArgumentException("a e/ou b menor(es) que zero");
-        }
-
-        int i = 1;
-        int s = 0;
-
-        while (i <= b){
-            s = s + a;
-            i = i + 1;
-        }
-
-        return s;
     }
 }
